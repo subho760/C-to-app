c87526ed647165e410e4a3e9550276d410d981cc
subho760 <nightshadow5g@gmail.com>
2026-06-11 17:23:12 +0530

    Update App.xaml.cs

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,10 +1,9 @@
 namespace App;
 
-public partial class App : Application
+public class App : Application
 {
     public App()
     {
-        // Explicitly bypass dynamic generation and load the main page structure
         MainPage = new MainPage();
     }
 }
