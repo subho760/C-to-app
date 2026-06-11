bd7b607af24b6afeee1876ad41ae1699012253a1
subho760 <nightshadow5g@gmail.com>
2026-06-11 20:23:01 +0530

    Update MainActivity.cs

--- src/MainActivity.cs
+++ src/MainActivity.cs
@@ -1,17 +1,21 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Microsoft.Maui.Embedding;
 
 namespace App;
 
 [Activity(
     Theme = "@style/Maui.MainTheme.NoActionBar", 
-    MainLauncher = true, // 🚀 THIS makes it show up in your app drawer!
+    MainLauncher = true, 
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
     protected override void OnCreate(Bundle? savedInstanceState)
     {
+        var mauiApp = MauiProgram.CreateMauiApp();
+        var mauiContext = new MauiContext(mauiApp.Services, this);
+        
         base.OnCreate(savedInstanceState);
     }
 }
